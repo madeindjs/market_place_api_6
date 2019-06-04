5c74377a68219b0dbb223b25015ebf29974e7ea9
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-04 13:44:02 +0200

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-
   def show
     render json: User.find(params[:id])
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
