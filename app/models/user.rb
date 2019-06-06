080e4e2b2396ead1ccf25bc5b7c33b4fe8942612
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-06 13:33:12 +0200

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
+
+  has_secure_password
 end
