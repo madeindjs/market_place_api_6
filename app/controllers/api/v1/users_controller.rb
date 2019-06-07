9f0ac9913deedac773f96f43613f13af5f5b9c3e
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-07 13:59:44 +0200

    Adds authorization for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   def show
     render json: @user
@@ -29,6 +30,10 @@ class Api::V1::UsersController < ApplicationController
   end
 
   private
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
+  end
 
   # Only allow a trusted parameter "white list" through.
   def user_params
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -28,19 +28,29 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    patch api_v1_user_url(@user),
+      params: { user: { email: @user.email } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+      as: :json
     assert_response :success
   end
 
-  test "should not update other user" do
-    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
-    assert_response :unprocessable_entity
+  test "should forbid update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete api_v1_user_url(@user), as: :json
+      delete api_v1_user_url(@user), headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
     end
     assert_response :no_content
+  end
+
+  test "should forbid destroy user" do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 end
