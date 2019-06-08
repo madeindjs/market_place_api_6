cdb285a8c242c4c8900c66ba6990208bf464aa5c
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-08 23:25:34 +0200

    Adds some validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :user_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   belongs_to :user
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  test "Should have a positive price" do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
 end
