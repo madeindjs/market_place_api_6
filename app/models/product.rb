7c6394117e0b26f6a0ade539e0998d036d636624
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-11 14:09:29 +0200

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,20 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   belongs_to :user
+
+  scope :filter_by_title, lambda { |keyword|
+    where('lower(title) LIKE ?', "%#{keyword.downcase}%")
+  }
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where('price >= ?', price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where('price <= ?', price)
+  }
+
+  scope :recent, lambda {
+    order(:updated_at)
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,27 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('TV').count
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:another_tv), products(:one)], Product.filter_by_title('TV').sort
+  end
+
+  test 'should filter products by price upper and sort them' do
+    assert_equal [products(:two), products(:one)], Product.above_or_equal_to_price(200).sort
+  end
+
+  test 'should filter products by price lower and sort them' do
+    assert_equal [products(:another_tv)], Product.below_or_equal_to_price(200).sort
+  end
+
+  test 'should sort product by most recent' do
+    # we will touch some products to update them
+    products(:two).touch
+    products(:one)
+    assert_equal [products(:another_tv), products(:one), products(:two)], Product.recent.to_a
+  end
 end
