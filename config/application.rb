f98530be3e719a9491e22bc46ca8e1c0f1b29d07
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-01-09 13:07:14 +0100

    Correct autoloading in production environment

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module MarketPlaceApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
