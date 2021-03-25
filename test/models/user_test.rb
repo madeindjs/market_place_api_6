1ceb337e3009e63dc24c7fe724809f6ddf1fe57a
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-03-25 22:15:18 +0100

    improve user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'user with a valid email should be valid' do
-    user = User.new(email: 'test@test.org', password_digest: 'test')
+    user = User.new(email: 'test@test.org', password: 'test')
     assert user.valid?
   end
 
   test 'user with unvalid email should be unvalid' do
-    user = User.new(email: 'test', password_digest: 'test')
+    user = User.new(email: 'test', password: 'test')
     assert_not user.valid?
   end
 
   test 'user with taken email should be unvalid' do
     other_user = users(:one)
-    user = User.new(email: other_user.email, password_digest: 'test')
+    user = User.new(email: other_user.email, password: 'test')
     assert_not user.valid?
   end
 
